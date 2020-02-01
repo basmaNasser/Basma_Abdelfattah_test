2a40f129c74ebf34fcd6c4335aa0225fcb564420
basmaNasser <basmaNasser@users.noreply.github.com>
2020-02-01 12:06:20 +0200

    README.md
    ## Q3
    
    ### Description
    
    At Ormuco, we want to optimize every bits of software we write. Your goal is to write a new library that can be integrated to the Ormuco stack. Dealing with network issues everyday, latency is our biggest problem. Thus, your challenge is to write a new Geo Distributed LRU (Least Recently Used) cache with time expiration. 
    
    ### The Answer
    
    1- Define cache size.
    2- I used OrderedDict to have ordered hash with key and value for the cache item.
    3- first will remove the rare item from the queue.
    4- set the new item in the front of the queue.
    5- if cache size exceeded will clear the cache queue.
    6- we set time for cache expiry, when reach cache expiry duration the cache will be cleared then new item will be added.
    7- if the same page/item recalled with the same key or different key it will be moved to the front of the queue
     
    source directory: './q3_cache'
    python file: LRU.py
    
    ### Example:
    ````
    Input parameters:  cache max size and expiry time to clear the cache
    
    s=LRU(5,110)
    s['a']=1
    s['c']=2
    s['d']=3
    s['r']=4
    s['d']=1
    s['a']=9
    s['j']=1
    s['d']=3
    s['r']=4
    s['d']=1
    s['a']=9
    s['d']=3
    s['r']=4
    print (s.cache)
    ````
    
    ### Installation
    ````
    Download LRU.py from https://github.com/basmaNasser/Basma_Abdelfattah_test/q3_cache
    import LRU 
    ````

---
+++ q3_cache/LRU.py
@@ -0,0 +1,63 @@
+import collections
+import time
+from threading import Timer
+
+class LRU:
+    def __init__(self, size,clear_time):
+        self.size = size
+        self.cache = collections.OrderedDict()  #to generate key and value for cache items
+        self.start_time=time.time()
+        self.clear_time=clear_time
+    #retrieve previous set data    
+    def __getitem__(self, k):
+            value = self.cache.pop(k)
+            self.cache[k] = value
+            return value
+        
+    # set the cache value and key    
+    def __setitem__(self, k, value):
+            try:
+               self.cache.pop(k)
+            except KeyError:
+                if len(self.cache) >= self.size:
+                    #remove the least used cache from the rare of the queue
+                    #if exceed the assigned size
+                    self.cache.popitem(last=False)                 
+                       
+            current_time = time.time()
+            if (round(current_time-self.start_time)) == self.clear_time:
+                self.timer()
+                self.cache[k] = value
+            else:
+                self.cache[k] = value
+                
+            
+    #clear cache        
+    def clearCache(self):
+       self.cache.clear()
+       
+    def timer(self):
+         Timer(self.clear_time,self.clearCache,()).start()
+         
+
+s=LRU(5,110)
+s['a']=1
+s['c']=2
+s['d']=3
+s['r']=4
+s['d']=1
+s['a']=9
+s['j']=1
+s['d']=3
+s['r']=4
+s['d']=1
+s['a']=9
+s['d']=3
+s['r']=4
+
+
+print (s.cache)
+
+
+
+
